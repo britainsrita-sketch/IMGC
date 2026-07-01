8e36fc873f23302bd5e40bc09258ba872bceff8a
britainsrita-sketch <britainsrita@gmail.com>
2026-07-01 08:06:49 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "==> Installing dependencies..."
-pip3 install python-telegram-bot==20.7 -q
+pip install -r requirements.txt --break-system-packages
 echo "==> Starting bot..."
 python3 bot.py
